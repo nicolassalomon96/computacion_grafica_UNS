444462d18a69ab1c9cacddda7759b52d7f90b37a
Nicolas Salomon <nicolassalomon96@gmail.com>
2022-11-11 16:49:21 -0300

    Subido phong shading

---
+++ tp7_lighting/phong-shading/ejercicio.js
@@ -0,0 +1,243 @@
+// <============================================ EJERCICIOS ============================================>
+// a) Implementar la función:
+//
+//      GetModelViewMatrix( translationX, translationY, translationZ, rotationX, rotationY )
+//
+//    Si la implementación es correcta, podrán hacer rotar la caja correctamente (como en el video). Notar 
+//    que esta función no es exactamente la misma que implementaron en el TP4, ya que no recibe por parámetro
+//    la matriz de proyección. Es decir, deberá retornar solo la transformación antes de la proyección model-view (MV)
+//    Es necesario completar esta implementación para que funcione el control de la luz en la interfaz. 
+//    IMPORTANTE: No es recomendable avanzar con los ejercicios b) y c) si este no funciona correctamente. 
+//
+// b) Implementar los métodos:
+//
+//      setMesh( vertPos, texCoords, normals )
+//      draw( matrixMVP, matrixMV, matrixNormal )
+//
+//    Si la implementación es correcta, podrán visualizar el objeto 3D que hayan cargado. Notar que es necesario pasar las 
+//    normales como atributo al VertexShader. 
+//    La función draw recibe ahora 3 matrices en column-major: 
+//
+//       * model-view-projection (MVP de 4x4)
+//       * model-view (MV de 4x4)
+//
+//  
+// d) Implementar los métodos:
+//
+//      setLightDir(x,y,z)
+//      setShininess(alpha)
+//    
+//    Estas funciones se llaman cada vez que se modifican los parámetros del modelo de iluminación en la 
+//    interface. No es necesario transformar la dirección de la luz (x,y,z), ya viene en espacio cámara.
+//
+// Otras aclaraciones: 
+//
+//      * Utilizaremos una sola fuente de luz direccional en toda la escena
+//      * La intensidad I para el modelo de iluminación debe ser seteada como blanca (1.0,1.0,1.0,1.0) en RGB
+//      * Es opcional incorporar la componente ambiental (Ka) del modelo de iluminación
+//      * Los coeficientes Kd y Ks correspondientes a las componentes difusa y especular del modelo 
+//        deben ser seteados con el color blanco. 
+//        
+// <=====================================================================================================>
+
+// Esta función recibe la matriz de proyección (ya calculada), una 
+// traslación y dos ángulos de rotación (en radianes). Cada una de 
+// las rotaciones se aplican sobre el eje x e y, respectivamente. 
+// La función debe retornar la combinación de las transformaciones 
+// 3D (rotación, traslación y proyección) en una matriz de 4x4, 
+// representada por un arreglo en formato column-major. 
+
+function GetModelViewMatrix( translationX, translationY, translationZ, rotationX, rotationY )
+{
+	
+	var rx = [1,					0,					 0,  0,
+			  0,  Math.cos(rotationX), Math.sin(rotationX),  0,
+			  0, -Math.sin(rotationX), Math.cos(rotationX),  0,
+			  0,					0,					 0,  1];
+
+	var ry = [Math.cos(rotationY),  0, -Math.sin(rotationY),  0,
+								0,  1,					  0,  0,
+			  Math.sin(rotationY),  0,  Math.cos(rotationY),  0,
+			  					0,  0,		              0,  1];
+
+	// Matriz de traslación
+	var trans = [			1,			  0,			0, 0,
+				 			0, 			  1,			0, 0,
+				 			0, 			  0,			1, 0,
+				 translationX, translationY, translationZ, 1];
+
+	// [COMPLETAR] Modificar el código para formar la matriz de transformación.
+	var mv = MatrixMult(MatrixMult(trans, ry),rx);
+	return mv;
+}
+
+// [COMPLETAR] Completar la implementación de esta clase.
+class MeshDrawer
+{
+	// El constructor es donde nos encargamos de realizar las inicializaciones necesarias. 
+	constructor()
+	{
+		// [COMPLETAR] inicializaciones
+
+		// 1. Compilamos el programa de shaders
+		this.prog = InitShaderProgram( meshVS, meshFS );
+		
+		// 2. Obtenemos los IDs de las variables uniformes en los shaders
+		this.mvp_loc = gl.getUniformLocation( this.prog, 'mvp' );
+		this.mv_loc = gl.getUniformLocation( this.prog, 'mv' );
+		this.light_dir = gl.getUniformLocation(this.prog, "light_dir")
+		this.shininess = gl.getUniformLocation(this.prog, "shininess");
+
+		// 3. Obtenemos los IDs de los atributos de los vértices en los shaders
+		this.pos_loc = gl.getAttribLocation( this.prog, 'pos' );
+		this.normal_loc = gl.getAttribLocation( this.prog, 'normal' );
+
+		// 4. Creamos los buffers
+		this.pos_buffer = gl.createBuffer();
+		this.normal_buffer = gl.createBuffer();
+
+	}
+	
+	// Esta función se llama cada vez que el usuario carga un nuevo
+	// archivo OBJ. En los argumentos de esta función llegan un areglo
+	// con las posiciones 3D de los vértices, un arreglo 2D con las
+	// coordenadas de textura y las normales correspondientes a cada 
+	// vértice. Todos los items en estos arreglos son del tipo float. 
+	// Los vértices y normales se componen de a tres elementos 
+	// consecutivos en el arreglo vertPos [x0,y0,z0,x1,y1,z1,..] y 
+	// normals [n0,n0,n0,n1,n1,n1,...]. 
+	setMesh( vertPos, texCoords, normals )
+	{
+		// [COMPLETAR] Actualizar el contenido del buffer de vértices y otros atributos..
+		this.numTriangles = vertPos.length / 3 / 3;
+
+		// 1. Binding y seteo del buffer de posiciones
+		gl.bindBuffer( gl.ARRAY_BUFFER, this.pos_buffer );
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertPos), gl.STATIC_DRAW);
+		
+		// 2. Binding y seteo del buffer de normales	
+		gl.bindBuffer( gl.ARRAY_BUFFER, this.normal_buffer );
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+	}
+	
+	
+	// Esta función se llama para dibujar la malla de triángulos
+	// El argumento es la matriz model-view-projection (matrixMVP) y
+	// la matriz model-view (matrixMV) que es retornada por 
+	// GetModelViewProjection
+	draw( matrixMVP, matrixMV )
+	{
+		// [COMPLETAR] Completar con lo necesario para dibujar la colección de triángulos en WebGL
+		
+		// 1. Seleccionamos el shader
+		gl.useProgram( this.prog );
+
+		// 2. Setear uniformes con las matrices de transformaciones
+		gl.uniformMatrix4fv( this.mvp_loc, false, matrixMVP );
+		gl.uniformMatrix4fv( this.mv_loc, false, matrixMV );
+
+   		// 3. Habilitar atributos: vértices, normales, texturas
+		gl.bindBuffer( gl.ARRAY_BUFFER, this.pos_buffer );
+		gl.vertexAttribPointer( this.pos_loc, 3, gl.FLOAT, false, 0, 0 );
+		gl.enableVertexAttribArray( this.pos_loc );
+
+		// 4 Binding del buffer de color
+		gl.bindBuffer( gl.ARRAY_BUFFER, this.normal_buffer );
+		gl.vertexAttribPointer( this.normal_loc, 3, gl.FLOAT, false, 0, 0 );
+		gl.enableVertexAttribArray( this.normal_loc );
+
+
+		gl.drawArrays( gl.TRIANGLES, 0, this.numTriangles * 3 );
+	}
+	
+	
+	// Este método se llama al actualizar la dirección de la luz desde la interfaz
+	setLightDir( x, y, z )
+	{		
+		// [COMPLETAR] Setear variables uniformes en el fragment shader para especificar la dirección de la luz
+		gl.useProgram(this.prog)
+		gl.uniform3f(this.light_dir, x, y, z)
+	}
+		
+	// Este método se llama al actualizar el brillo del material 
+	setShininess( shininess )
+	{		
+		// [COMPLETAR] Setear variables uniformes en el fragment shader para especificar el brillo.
+		gl.useProgram(this.prog);
+		gl.uniform1f(this.shininess, shininess);
+	}
+}
+
+
+
+// [COMPLETAR] Calcular iluminación utilizando modelo de iluminación de Phong y
+// shading de Phong o Gouraud. Elegir inicialmente 1, una vez que está funcionando
+// duplicar el proyecto e implementar el restante.
+
+// Recordar que: 
+// Si declarás las variables pero no las usás, es como que no las declaraste
+// y va a tirar error. Siempre va punto y coma al finalizar la sentencia. 
+// Las constantes en punto flotante necesitan ser expresadas como x.y, 
+// incluso si son enteros: ejemplo, para 4 escribimos 4.0.
+
+// Ayuda: Computen los vectores N, L, R, V normalizados antes de computar la iluminación
+
+// Algunas funciones útiles para escribir los shader:
+// Dot product: https://thebookofshaders.com/glossary/?search=dot
+// Normalize:   https://thebookofshaders.com/glossary/?search=normalize
+// Pow:         https://thebookofshaders.com/glossary/?search=pow
+// Clamp:       https://thebookofshaders.com/glossary/?search=clamp
+
+// Vertex Shader
+var meshVS = `
+	attribute vec3 pos;
+	attribute vec3 normal;
+
+	uniform mat4 mvp;
+	uniform mat4 mv;
+
+	varying vec3 normal2fs_eye;
+	varying vec3 pos2fs_eye;
+
+	void main()
+	{ 
+		normal2fs_eye = (mv * vec4(normal, 0.0)).xyz;
+		pos2fs_eye = (mv * vec4(pos, 1.0)).xyz;
+
+		gl_Position = mvp * vec4(pos,1);
+	}
+`;
+
+var meshFS = `
+	precision mediump float;
+
+	uniform vec3 light_dir;
+	uniform float shininess;
+
+	varying vec3 normal2fs_eye;
+	varying vec3 pos2fs_eye;
+
+	void main()
+	{		
+		vec3 N = normalize(normal2fs_eye);
+		vec3 L = normalize(light_dir);
+		vec3 R = normalize(reflect(-L, N));
+		vec3 V = normalize(-pos2fs_eye);
+
+		vec3 light_color = vec3(1, 1, 1);
+		vec3 obj_color = vec3(0.4, 0, 1.0);
+		vec3 ka = vec3(0.1, 0.1, 0.1);
+
+		vec3 surface_color = obj_color  * vec3( max(dot(L, N), 0.0) ) + vec3( pow( max(0.0, dot(R, V)), shininess)) + ka * light_color;
+
+		gl_FragColor = vec4( surface_color, 1 );
+	}
+`;
+
+
+
+
+
+
+
+
